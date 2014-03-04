fba8174dd53ce43c240af4d4b3f6924f6fa2bf90
Mike Stubna <mike@stubna.com>
2014-03-04 15:00:53 -0500

    updating config file

--- config.rb
+++ config.rb
@@ -3,14 +3,10 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'stylesheets/fonts'
 
-after_configuration do
-  sprockets.append_path 'vendor'
-  sprockets.append_path 'data'
-end
-
 # layouts
 page "example_1.html", layout: "example_layout"
 page "example_2.html", layout: "example_layout"
+page "example_3.html", layout: "example_layout"
 
 # Development-specific configuration
 configure :development do
@@ -20,4 +16,5 @@ end
 # Build-specific configuration
 configure :build do
   activate :relative_assets
+  set :relative_links, true
 end
